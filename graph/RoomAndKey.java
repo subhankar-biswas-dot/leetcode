ca221fe62d1fb5eac610a28fd4341a94e34a2b89
Subhankar biswas <subhankarbiswas30000@gmail.com>
2022-01-15 00:12:11 +0530

    leet code 841

--- graph/RoomAndKey.java
+++ graph/RoomAndKey.java
@@ -8,17 +8,18 @@ import java.util.List;
 public class RoomAndKey {
     public static void main(String[] args) {
         List<List<Integer>> rooms = Arrays.asList(Arrays.asList(1,3),Arrays.asList(3,0,1),Arrays.asList(2),Arrays.asList(0));
-        List<Boolean> visited=new ArrayList(Collections.nCopies(rooms.size(), false));       
-        BFS(rooms,visited,0);
+        List<Boolean> visited=new ArrayList(Collections.nCopies(rooms.size(), false));
+        List<Integer>res=new ArrayList<>();      
+        BFS(rooms,visited,0,res);
         
     }
-    public static void BFS(List<List<Integer>> rooms,List<Boolean> visited,int src){
+    public static void BFS(List<List<Integer>> rooms,List<Boolean> visited,int src,List<Integer> res){
         LinkedList<Integer> queue = new LinkedList<>();
         queue.add(src);
         visited.set(src,true);
         while(!queue.isEmpty()){
             int s = queue.poll();
-            System.out.println(s);
+            res.add(s);
             List<Integer> tmp = rooms.get(s);
             for(int i=0;i<tmp.size();i++){
                 if(!visited.get(tmp.get(i))){
